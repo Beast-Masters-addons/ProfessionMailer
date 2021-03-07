a1ec1d6e3634a8c17712e68f9b010f30e9dd6b38
datagutten <datagutten@datagutten.net>
2021-03-07 09:56:13 +0100

    Add test for ProfessionData

---
+++ tests/test_ProfessionData.lua
@@ -0,0 +1,43 @@
+local lu = require('luaunit')
+
+
+loadfile('test_data2.lua')()
+loadfile('wow_functions.lua')()
+loadfile('../libs/BM-utils/addon/version.lua')()
+loadfile('../libs/BM-utils/addon/utils.lua')()
+loadfile('../addon/ProfessionData.lua')()
+
+
+_G['test'] = {}
+local test = _G['test']
+local ProfessionData =_G['ProfessionData']
+
+function test:testItemUsedFor()
+	local usages = ProfessionData:ItemUsedFor(118)
+	lu.assertEquals(usages,  {[858] = {
+		["name"] = "Lesser Healing Potion",
+		["itemID"] = 858,
+	}})
+end
+
+function test:testWhoNeeds()
+	local needs = ProfessionData:whoNeeds(2447)
+	lu.assertEquals(needs[1]['name'], 'Elixir of Minor Fortitude')
+	lu.assertEquals(needs[1]['craftedItemId'], 2458)
+	lu.assertEquals(needs[1]['character'], 'Quadbear-Mirage Raceway')
+end
+
+function test:testWhoNeeds2()
+	local needs = ProfessionData:whoNeeds(2678)
+	lu.assertEquals(needs[2]['name'], 'Herb Baked Egg')
+	lu.assertEquals(needs[2]['craftedItemId'], 6888)
+	lu.assertEquals(needs[2]['character'], 'Quadbear-Mirage Raceway')
+end
+
+function test:testWhoNeedsNone()
+	local needs = ProfessionData:whoNeeds(3280)
+	lu.assertNil(needs)
+end
+
+
+os.exit( lu.LuaUnit.run() )
---
+++ tests/test_data2.lua
@@ -0,0 +1,250 @@
+
+ItemRecipes = {
+	[3173] = {
+		[3220] = {
+			["name"] = "Blood Sausage",
+			["itemID"] = 3220,
+		},
+	},
+	[118] = {
+		[858] = {
+			["name"] = "Lesser Healing Potion",
+			["itemID"] = 858,
+		},
+	},
+	[6358] = {
+		[6370] = {
+			["name"] = "Blackmouth Oil",
+			["itemID"] = 6370,
+		},
+	},
+	[6889] = {
+		[6888] = {
+			["name"] = "Herb Baked Egg",
+			["itemID"] = 6888,
+		},
+	},
+	[3371] = {
+		[118] = {
+			["name"] = "Minor Healing Potion",
+			["itemID"] = 118,
+		},
+		[2454] = {
+			["name"] = "Elixir of Lion's Strength",
+			["itemID"] = 2454,
+		},
+		[3385] = {
+			["name"] = "Lesser Mana Potion",
+			["itemID"] = 3385,
+		},
+		[2456] = {
+			["name"] = "Minor Rejuvenation Potion",
+			["itemID"] = 2456,
+		},
+		[2458] = {
+			["name"] = "Elixir of Minor Fortitude",
+			["itemID"] = 2458,
+		},
+		[5996] = {
+			["name"] = "Elixir of Water Breathing",
+			["itemID"] = 5996,
+		},
+		[6372] = {
+			["name"] = "Swim Speed Potion",
+			["itemID"] = 6372,
+		},
+		[3382] = {
+			["name"] = "Weak Troll's Blood Potion",
+			["itemID"] = 3382,
+		},
+		[2455] = {
+			["name"] = "Minor Mana Potion",
+			["itemID"] = 2455,
+		},
+		[5997] = {
+			["name"] = "Elixir of Minor Defense",
+			["itemID"] = 5997,
+		},
+		[6370] = {
+			["name"] = "Blackmouth Oil",
+			["itemID"] = 6370,
+		},
+	},
+	[6370] = {
+		[6372] = {
+			["name"] = "Swim Speed Potion",
+			["itemID"] = 6372,
+		},
+		[5996] = {
+			["name"] = "Elixir of Water Breathing",
+			["itemID"] = 5996,
+		},
+	},
+	[2447] = {
+		[2458] = {
+			["name"] = "Elixir of Minor Fortitude",
+			["itemID"] = 2458,
+		},
+		[3382] = {
+			["name"] = "Weak Troll's Blood Potion",
+			["itemID"] = 3382,
+		},
+		[2456] = {
+			["name"] = "Minor Rejuvenation Potion",
+			["itemID"] = 2456,
+		},
+		[118] = {
+			["name"] = "Minor Healing Potion",
+			["itemID"] = 118,
+		},
+	},
+	[2449] = {
+		[3382] = {
+			["name"] = "Weak Troll's Blood Potion",
+			["itemID"] = 3382,
+		},
+		[2454] = {
+			["name"] = "Elixir of Lion's Strength",
+			["itemID"] = 2454,
+		},
+		[2458] = {
+			["name"] = "Elixir of Minor Fortitude",
+			["itemID"] = 2458,
+		},
+	},
+	[730] = {
+		[733] = {
+			["name"] = "Westfall Stew",
+			["itemID"] = 733,
+		},
+	},
+	[731] = {
+		[733] = {
+			["name"] = "Westfall Stew",
+			["itemID"] = 733,
+		},
+	},
+	[3172] = {
+		[3220] = {
+			["name"] = "Blood Sausage",
+			["itemID"] = 3220,
+		},
+	},
+	[2678] = {
+		[724] = {
+			["name"] = "Goretusk Liver Pie",
+			["itemID"] = 724,
+		},
+		[6888] = {
+			["name"] = "Herb Baked Egg",
+			["itemID"] = 6888,
+		},
+	},
+	[765] = {
+		[2455] = {
+			["name"] = "Minor Mana Potion",
+			["itemID"] = 2455,
+		},
+		[2454] = {
+			["name"] = "Elixir of Lion's Strength",
+			["itemID"] = 2454,
+		},
+		[5997] = {
+			["name"] = "Elixir of Minor Defense",
+			["itemID"] = 5997,
+		},
+		[118] = {
+			["name"] = "Minor Healing Potion",
+			["itemID"] = 118,
+		},
+	},
+	[5465] = {
+		[5472] = {
+			["name"] = "Kaldorei Spider Kabob",
+			["itemID"] = 5472,
+		},
+	},
+	[2672] = {
+		[2679] = {
+			["name"] = "Charred Wolf Meat",
+			["itemID"] = 2679,
+		},
+	},
+	[769] = {
+		[2681] = {
+			["name"] = "Roasted Boar Meat",
+			["itemID"] = 2681,
+		},
+	},
+	[723] = {
+		[724] = {
+			["name"] = "Goretusk Liver Pie",
+			["itemID"] = 724,
+		},
+	},
+	[2450] = {
+		[858] = {
+			["name"] = "Lesser Healing Potion",
+			["itemID"] = 858,
+		},
+	},
+	[2452] = {
+		[6372] = {
+			["name"] = "Swim Speed Potion",
+			["itemID"] = 6372,
+		},
+	},
+	[785] = {
+		[2455] = {
+			["name"] = "Minor Mana Potion",
+			["itemID"] = 2455,
+		},
+		[2456] = {
+			["name"] = "Minor Rejuvenation Potion",
+			["itemID"] = 2456,
+		},
+		[3385] = {
+			["name"] = "Lesser Mana Potion",
+			["itemID"] = 3385,
+		},
+	},
+	[3820] = {
+		[3385] = {
+			["name"] = "Lesser Mana Potion",
+			["itemID"] = 3385,
+		},
+		[5996] = {
+			["name"] = "Elixir of Water Breathing",
+			["itemID"] = 5996,
+		},
+	},
+	[729] = {
+		[733] = {
+			["name"] = "Westfall Stew",
+			["itemID"] = 733,
+		},
+	},
+	[3174] = {
+		[3220] = {
+			["name"] = "Blood Sausage",
+			["itemID"] = 3220,
+		},
+	},
+}
+CharacterDifficulty = {
+	["Quadbear-Mirage Raceway"] = {
+		[733] = "optimal",
+		[3220] = "optimal",
+		[2679] = "trivial",
+		[2681] = "trivial",
+		[5472] = "trivial",
+		[724] = "medium",
+		[6888] = "trivial",
+		[2458] = "trivial",
+	},
+}
+CharacterNeeds = {
+	["Quadbear-Mirage Raceway"] = {
+	},
+}
+CharacterProfessions = nil
